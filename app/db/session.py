f673ad6341d532f79b4219e09c10c6b117282079
jeshin <jsshin0201@gmail.com>
2025-07-21 11:25:58 +0900

    feat: password 특수문자 가능하도록 인코딩사용

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
+from urllib.parse import quote_plus
 
-DATABASE_URL = f"mysql+aiomysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+password = quote_plus(settings.DB_PASSWORD)
+
+DATABASE_URL = (
+    f"mysql+aiomysql://{settings.DB_USER}:{password}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
 
 engine = create_async_engine(DATABASE_URL, echo=False)  # echo=False로 SQL 로그 비활성화
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
